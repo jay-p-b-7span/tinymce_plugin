fe0bcd612b084934543cc9ec062bb6305b55d91f
jay-p-b-7span <jay.p.b@7span.com>
2023-07-24 17:13:02 +0530

    plugin is now working. now making directus cloudinary config dynamic

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
 # cloudinary TinyMCE Plugin
 
 Welcome stranger! This is a repo containing the cloudinary TinyMCE plugin.
+
+### Response from cloudinary after selecting Asset
+
+https://cloudinary.com/documentation/media_library_widget
+
+```json
+{
+  "public_id": "sample",
+  "resource_type": "image",
+  "type": "upload",
+  "format": "jpg",
+  "version": 1511474034,
+  "url": "http://res.cloudinary.com/demo/image/upload/v1511474034/sample.jpg",
+  "secure_url": "https://res.cloudinary.com/demo/image/upload/v1511474034/sample.jpg",
+  "width": 864,
+  "height": 576,
+  "bytes": 120257,
+  "duration": null,
+  "tags": [],
+  "metadata": [],
+  "created_at": "2023-02-08T08:38:51Z",
+  "derived": [
+    {
+      "url": "http://res.cloudinary.com/demo/image/upload/c_scale,e_grayscale,f_auto,q_auto,w_100/v1511474034/sample.jpg",
+      "secure_url": "https://res.cloudinary.com/demo/image/upload/c_scale,e_grayscale,f_auto,q_auto,w_100/v1511474034/sample.jpg"
+    }
+  ],
+  "access_mode": "public",
+  "access_control": [],
+  "created_by": {
+    "type": "user",
+    "id": "abc123def456ghiabc123def456ghi"
+  },
+  "uploaded_by": {
+    "type": "user",
+    "id": "xyz789efg456qrsxyz789efg456qrs"
+  }
+```
--- src/main/ts/Plugin.ts
+++ src/main/ts/Plugin.ts
@@ -1,12 +1,61 @@
 import { Editor, TinyMCE } from 'tinymce';
 
 declare const tinymce: TinyMCE;
+const library_widget_config = {
+  cloud_name: "dmparrmdk",
+  api_key: "353997448357236",
+  remove_header: false,
+  max_files: "1",
+  insert_caption: "Insert",
+  default_transformations: [[]],
+  button_class: "myBtn",
+  button_caption: "Select Image",
+};
 
+let libraryWidget = null;
+console.log("🚀 ~ file: Plugin.ts:17 ~ libraryWidget:", libraryWidget)
+let editorInstance = null;
+const onSelectFromGallery = (response) =>  {
+  console.log("🚀 ~ file: Plugin.ts:19 ~ onSelectFromGallery ~ response:", response)
+  if( response && response.assets.length ){
+    response.assets.forEach( ( asset ) => {
+      if( asset.resource_type === 'image'){
+        editorInstance.insertContent(`<img src=${asset.url}>`, { format : 'html'});
+      }else if( asset.resource_type === 'video'){
+        editorInstance.insertContent(`<video controls>
+        <source src=${asset.url} type=video/${asset.format}>
+        Your browser does not support the video tag.
+        </video>`);
+      }
+    });
+
+  }
+    
+  
+};
+
+      tinymce.ScriptLoader.loadScript('https://media-library.cloudinary.com/global/all.js').then( () => {
+        console.log('script loaded');
+
+        libraryWidget = cloudinary.createMediaLibrary(
+          library_widget_config,{
+                insertHandler: onSelectFromGallery,
+          }
+      );
+
+      });
+
+    
+
+    
 const setup = (editor: Editor, url: string): void => {
+  console.log("🚀 ~ file: Plugin.ts:46 ~ setup ~ url:", url)
+  editorInstance = editor;
   editor.ui.registry.addButton('cloudinary', {
-    text: 'cloudinary button',
+    icon : 'gallery',
+    tooltip : 'Cloudinary Library',
     onAction: () => {
-      editor.setContent('<p>content added from cloudinary</p>');
+      libraryWidget.show();
     }
   });
 };
